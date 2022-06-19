fixed rotation with perspective

linear_algebra/orthogonal_projection.py
@@ -78,7 +78,7 @@ while True:
     display.fill(BLACK)
 
     for index,point in enumerate(points):
-        rotated2d = np.dot(rotate_x,point.reshape((3,1)))
+        rotated2d = np.dot(rotate_y,point.reshape((3,1)))
         rotated2d = np.dot(rotate_y,rotated2d)
         rotated2d = np.dot(rotate_z,rotated2d)
         projected2d = np.dot(projection_matrix,rotated2d)

linear_algebra/perspective_projection.py
@@ -5,10 +5,10 @@ import numpy as np
 FPS = 60
 
 points = []
-bounds = (3,1)
+bounds = (-1,1)
 for x in bounds:
     for y in bounds:
-        for z in (0,2):
+        for z in (1,3):
             points.append(np.matrix([x,y,z,1]))
 
 projection_matrix = [
@@ -24,12 +24,17 @@ projected_points = [
 
 pygame.init()
 clock = pygame.time.Clock()
-
-WIDTH,HEIGHT = 1000,800
+font = pygame.font.Font('../font/joystix.ttf',20)
+WIDTH,HEIGHT = 800,800
 SCALE = 100;
 CENTER = [WIDTH/2,HEIGHT/2]
 angle = 0
-fov = pi/2
+fov = pi/3
+
+def debug(display, debug_msg, font):
+    text = font.render(debug_msg, False, 'blue')
+    text_box = text.get_rect(topleft = (100,100)).inflate(20, 20)
+    display.blit(text, text_box)
 
 def make_perspective(fov,aspect,znear,zfar):
     # make the perspective projection matrix
@@ -55,6 +60,16 @@ def multiply_projection(projection_matrix,pos):
     return result
 
 def rotate(angle,pos):
+    # apply a translation to origin before rotation
+    translate_to_origin = np.matrix([
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,-2],
+        [0,0,0,1]
+    ])
+    rotated2d = np.dot(translate_to_origin,pos)
+
+    # apply rotation
     rotate_x = np.matrix([
         [1,0,0,0],
         [0,cos(angle),-sin(angle),0],
@@ -76,10 +91,18 @@ def rotate(angle,pos):
         [0,0,0,1]
     ])
 
-    rotated2d = np.dot(rotate_z,pos)
-    # rotated2d = np.dot(rotate_y,rotated2d)
-    # rotated2d = np.dot(rotate_z,rotated2d)
+    rotated2d = np.dot(rotate_x,rotated2d)
+    rotated2d = np.dot(rotate_y,rotated2d)
+    rotated2d = np.dot(rotate_z,rotated2d)
 
+    # apply translation back to the original position
+    translate_to_pos = np.matrix([
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,2],
+        [0,0,0,1]
+    ])
+    rotated2d = np.dot(translate_to_pos,rotated2d)
     return rotated2d
 
 projection_matrix = make_perspective(fov,HEIGHT/WIDTH,0,4)
@@ -96,9 +119,18 @@ YELLOW = (255,255,0)
 display = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("cube with perspective")
 
-def connect_points(a,b,c,d,col,points):
+def draw_planes(a,b,c,d,col,points):
     pygame.draw.polygon(display,col,[(points[a][0],points[a][1]),(points[b][0],points[b][1]),(points[c][0],points[c][1]),(points[d][0],points[d][1])])
-    # pygame.draw.line(display,WHITE,(points[i][0],points[i][1]),(points[j][0],points[j][1]))
+
+def connect_points(i,j,points):
+    if i==0 or j==0:
+        pygame.draw.line(display,RED,(points[i][0],points[i][1]),(points[j][0],points[j][1]))
+    elif i==3 or j==3:
+        pygame.draw.line(display,GREEN,(points[i][0],points[i][1]),(points[j][0],points[j][1]))
+    elif i==5 or j==5:
+        pygame.draw.line(display,BLUE,(points[i][0],points[i][1]),(points[j][0],points[j][1]))
+    else:
+        pygame.draw.line(display,WHITE,(points[i][0],points[i][1]),(points[j][0],points[j][1]))
 
 while True:
     clock.tick(FPS)
@@ -112,9 +144,17 @@ while True:
                 pygame.quit()
                 sys.exit()
     
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        angle+=0.01
+    elif keys[pygame.K_LEFT]:
+        angle-=0.01
+    else:
+        angle+=0.01
+    
     display.fill(BLACK)
 
-    angle+=0.01
+    debug(display,str(angle),font)
 
     for index,point in enumerate(points):
         rotated2d = rotate(angle,point.reshape((4,1)))
@@ -124,16 +164,11 @@ while True:
         projected_points[index] = [x,y]
         pygame.draw.circle(display,WHITE,(x,y),5)
 
-    connect_points(1,3,7,5,YELLOW,projected_points)
-    if abs(bounds[0])<abs(bounds[1]):
-        connect_points(4,5,7,6,GREEN,projected_points)
-        connect_points(2,3,7,6,CYAN,projected_points)
-        connect_points(0,1,5,4,BLUE,projected_points)
-        connect_points(0,1,3,2,RED,projected_points)
-    else:
-        connect_points(0,1,3,2,RED,projected_points)
-        connect_points(0,1,5,4,BLUE,projected_points)
-        connect_points(4,5,7,6,GREEN,projected_points)
-        connect_points(2,3,7,6,CYAN,projected_points)
-    connect_points(0,2,6,4,MAGENTA,projected_points)
+    for i in (0,2,4,6):
+        connect_points(i,i+1,projected_points)
+    for i in (0,1,4,5):
+        connect_points(i,i+2,projected_points)
+    for i in (0,1,2,3):
+        connect_points(i,i+4,projected_points)
+
     pygame.display.update()